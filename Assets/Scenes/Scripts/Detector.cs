1381137e637019f14712f9d7529ebb35d001f4a4
GabrielePrinciotta98 <gabriele.princiotta98@gmail.com>
2020-12-09 12:30:58 +0100

    First commit of prototype
    Feature included:
    - drag and drop control of atoms (to be improved)
    - custom render texture of rifraction patterns
    - spawn in blocks

---
+++ Assets/Scenes/Scripts/Detector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detector : MonoBehaviour
+{
+    private AtomsManager atomsManager;
+
+    private void Awake()
+    {
+        atomsManager = GameObject.FindObjectOfType<AtomsManager>();
+    }
+
+    private void Update()
+    {
+        Shader.SetGlobalVectorArray("centers", atomsManager.GetPositions());
+        Shader.SetGlobalInt("n_atoms", atomsManager.GetAtoms().Count);
+    
+
+    }
+
+
+}
