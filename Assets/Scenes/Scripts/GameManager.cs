26c12bdd2e5cfc756e5cd99a1b2478fcdea1c3d9
GabrielePrinciotta98 <gabriele.princiotta98@gmail.com>
2020-12-20 19:09:31 +0100

    Curtain animation + small tweaks

--- Assets/Scenes/Scripts/GameManager.cs
+++ Assets/Scenes/Scripts/GameManager.cs
@@ -52,18 +52,46 @@ public class GameManager : MonoBehaviour
 
     IEnumerator OpenCurtains()
     {
-        float newScaleX, newPosX;
+        /*
+        float newScaleX, newPosX, newScaleZ;
         newScaleX = curtain.transform.localScale.x;
+        newScaleZ = curtain.transform.localScale.z;
         newPosX = curtain.transform.localPosition.x;
         //curtain.SetActive(false);
         while (curtain.transform.localScale.x >= 2f)
         {
             newScaleX -= 0.1f;
+            newScaleZ += 0.1f;
             newPosX += 0.1f;
-            curtain.transform.localScale = new Vector3(newScaleX, curtain.transform.localScale.y, curtain.transform.localScale.z);
+            curtain.transform.localScale = new Vector3(newScaleX, curtain.transform.localScale.y, newScaleZ);
             curtain.transform.localPosition = new Vector3(newPosX, curtain.transform.localPosition.y, curtain.transform.localPosition.z);
-            yield return new WaitForSeconds(0.005f);
+            //yield return new WaitForSeconds(0.001f);
+            yield return new WaitForFixedUpdate();
         }
+        */
+
+        float t = 1;
+        float newScaleX, newPosX, newScaleZ;
+        newScaleX = curtain.transform.localScale.x;
+        newScaleZ = curtain.transform.localScale.z;
+        newPosX = curtain.transform.localPosition.x;
+        while (t >= 0) 
+        {
+            float ease = easeInOutBack(t);
+
+            newScaleX = Mathf.Lerp(2f, curtain.transform.localScale.x, ease);
+            newScaleZ = Mathf.Lerp(10f, curtain.transform.localScale.z, ease);
+            curtain.transform.localScale = new Vector3(newScaleX, curtain.transform.localScale.y, newScaleZ);
+            newPosX = Mathf.Lerp(45f, curtain.transform.localPosition.x, ease);
+            curtain.transform.localPosition = new Vector3(newPosX, curtain.transform.localPosition.y, curtain.transform.localPosition.z);
+
+            t -= 0.02f;
+            yield return new WaitForFixedUpdate();
+
+        }
+
+
+
     }
 
     public void HideSolution()
@@ -73,17 +101,60 @@ public class GameManager : MonoBehaviour
 
     IEnumerator CloseCurtains()
     {
-        float newScaleX, newPosX;
+        /*
+        float newScaleX, newPosX, newScaleZ;
         newScaleX = curtain.transform.localScale.x;
+        newScaleZ = curtain.transform.localScale.z;
         newPosX = curtain.transform.localPosition.x;
         //curtain.SetActive(false);
-        while (curtain.transform.localScale.x <= 18f)
+        while (curtain.transform.localScale.x < 16f)
         {
             newScaleX += 0.1f;
+            newScaleZ -= 0.1f;
             newPosX -= 0.1f;
-            curtain.transform.localScale = new Vector3(newScaleX, curtain.transform.localScale.y, curtain.transform.localScale.z);
+            curtain.transform.localScale = new Vector3(newScaleX, curtain.transform.localScale.y, newScaleZ);
             curtain.transform.localPosition = new Vector3(newPosX, curtain.transform.localPosition.y, curtain.transform.localPosition.z);
-            yield return new WaitForSeconds(0.005f);
+            //yield return new WaitForSeconds(0.001f);
+            yield return new WaitForFixedUpdate();
+
+        }
+        */
+
+        float t = 0;
+        float newScaleX, newPosX, newScaleZ;
+        newScaleX = curtain.transform.localScale.x;
+        newScaleZ = curtain.transform.localScale.z;
+        newPosX = curtain.transform.localPosition.x;
+        while (t <= 1)
+        {
+            float ease = easeInOutBack(t);
+
+            newScaleX = Mathf.Lerp(curtain.transform.localScale.x, 16f, ease);
+            newScaleZ = Mathf.Lerp( curtain.transform.localScale.z, 3f, ease);
+            curtain.transform.localScale = new Vector3(newScaleX, curtain.transform.localScale.y, newScaleZ);
+            newPosX = Mathf.Lerp(curtain.transform.localPosition.x, 33f, ease);
+            curtain.transform.localPosition = new Vector3(newPosX, curtain.transform.localPosition.y, curtain.transform.localPosition.z);
+            t += 0.02f;
+            yield return new WaitForFixedUpdate();
+
+        }
+
+    }
+
+    public float easeInOutBack(float x)
+    {
+        const float c1 = 1;//1.70158f;
+        const float c2 = c1 * 1.525f;
+
+        if (x < 0.5f)
+        {
+            return (Mathf.Pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2f;
+        }
+        else
+        {
+            return (Mathf.Pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2f;
         }
     }
+
+
 }
