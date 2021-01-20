8a6c219e44f88013f45df5c357ceb8fc918e9a82
GabrielePrinciotta98 <gabriele.princiotta98@gmail.com>
2021-01-20 18:07:26 +0100

    Emitter fixes

--- Assets/Scripts/EmitterCone.cs
+++ Assets/Scripts/EmitterCone.cs
@@ -6,12 +6,13 @@ using UnityEngine;
 
 public class EmitterCone : MonoBehaviour
 {
-    private Vector3 newScale;
+    private Vector3 newScale, newPos;
     private float zoom = 4f, power = 1f, lambda = 0.5f;
     private static readonly int EmissionColor = Shader.PropertyToID("_EmissionColor");
     private Renderer _renderer;
     private static readonly int _Color = Shader.PropertyToID("_Color");
-
+    private float zPos;
+    
     private void Start()
     {
         _renderer = GetComponentInChildren<Renderer>();
@@ -37,27 +38,29 @@ public class EmitterCone : MonoBehaviour
     private void Update()
     {
         newScale = transform.localScale;
-        
-        //DA SISTEMARE DOPO AVER FATTO IL MODELLO DELL'EMITTER
-        newScale.x = power/4 * Mathf.Sqrt(zoom);
-        newScale.z = power/4 * Mathf.Sqrt(zoom);
-        newScale.y = power/4 / Mathf.Sqrt(zoom);
-            
-         
-        
+        newPos = transform.localPosition;
         /*
-        newScale.x = 1f + 5f / 29f * (xz - 1f); 
-        newScale.y = 1.5f + 1.5f / 10f * y;
-        newScale.z = 1f + 5f / 29f * (xz - 1f); */
+        //DA SISTEMARE DOPO AVER FATTO IL MODELLO DELL'EMITTER
+        newScale.x = power/10 * Mathf.Sqrt(zoom);
+        newScale.z = power/10 * Mathf.Sqrt(zoom);
+        newScale.y = power/10 / Mathf.Sqrt(zoom);
+        */
+
+        newPos.z = 1f + 0.6f / 9f * (power - 1f); 
+        
+        newScale.x = 1 + 0.7f / 9f * (zoom - 1f) + 0.7f / 9f * (power - 1f); 
+        newScale.y = 1 + 0.7f / 9f * (power - 1f);
+        newScale.z = 1 + 0.7f / 9f * (zoom - 1f) + 0.7f / 9f * (power - 1f);
         transform.localScale = newScale;
+        transform.localPosition = newPos;
         SetEmissionColor();
     }
 
     private void SetEmissionColor()
     {
         _renderer.material.SetColor(_Color, 
-            Color.HSVToRGB(0.75f + -0.75f/1.7f*(lambda-0.3f), 1f, 1f, true));
+            Color.HSVToRGB(0.75f/1.7f*(lambda-0.3f), 1f, 1f, true));
         _renderer.material.SetColor(EmissionColor, 
-            Color.HSVToRGB(0.75f + -0.75f/1.7f*(lambda-0.3f), 1f, 1f, true));
+            Color.HSVToRGB(0.75f/1.7f*(lambda-0.3f), 1f, 1f, true));
     }
 }
